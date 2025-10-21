a8ac21171b9e00bc2c0acf73abac2191dc6771c7
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2025-10-22 02:04:54 +0300

    feat: add basic navbar on mobile

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,16 @@
+function NavBar() {
+  return (
+    // TODO Remove white bg
+    <header className="bg-white fixed w-full">
+      <nav className="flex justify-between px-4 py-6 items-center text-white *:mix-blend-exclusion *:uppercase">
+        <a href="#" className="w-min bold-text text-2xl leading-[0.75] ">
+          Ahmed Ibrahim
+        </a>
+        <a href="#" className="font-light">
+          Menu<sup>+</sup>
+        </a>
+      </nav>
+    </header>
+  );
+}
+export default NavBar;
