563d134ce3be5ba47dc915c3efcdf204005c5018
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2025-10-22 22:50:24 +0300

    feat: add hamburger menu basic UI

---
+++ src/components/HamburgerMenu.tsx
@@ -0,0 +1,66 @@
+import type { ReactNode } from "react";
+
+function HorizontalLinks({ links }: { links: ReactNode[] }) {
+  const items = links.map((item, index) => <li key={index}>{item}</li>);
+
+  return <ul className="flex justify-between">{items}</ul>;
+}
+
+function HamburgerMenu() {
+  return (
+    <nav
+      aria-label="Main navigation"
+      className="bg-white fixed inset-0 w-full h-full pt-19.5 px-4 flex flex-col pb-2"
+    >
+      {/* TODO: Add dynamic time zone and time */}
+      <div className="font-light text-xs text-right uppercase tracking-widest">
+        <div>Alexandria, Egypt:</div>
+        <div>(GMT+3) 18:42</div>
+      </div>
+      <ul className="uppercase text-5xl bold-text space-y-3 my-auto tracking-tighter">
+        <li>
+          <a href="#">About me</a>
+        </li>
+        <li>
+          <a href="#">Works</a>
+        </li>
+        <li>
+          <a href="#">Services</a>
+        </li>
+        <li>
+          <a href="#">Connect</a>
+        </li>
+      </ul>
+
+      <div className="space-y-4 mt-auto">
+        <HorizontalLinks
+          links={[
+            <a
+              href="https://www.linkedin.com/in/ahmed-ibrahim-807258243/"
+              target="_blank"
+              className="flex items-center gap-x-2 font-light uppercase"
+            >
+              [<span className="text-xs">LinkedIn</span>]
+            </a>,
+            <a
+              href="https://github.com/Ah-Ibrahim"
+              target="_blank"
+              className="flex items-center gap-x-2 font-light uppercase"
+            >
+              [<span className="text-xs">Github</span>]
+            </a>,
+          ]}
+        />
+        <div className="text-center bold-text text-2xl">
+          <a href="mailto:ahmed.ibrahim.elsayed01@gmail.com">
+            ahmed.ibrahim.elsayed01@gmail.com
+          </a>
+        </div>
+        <div className="text-sm text-center text-secondary">
+          &copy; All Rights Reserved. 2025 AhmedIbrahim
+        </div>
+      </div>
+    </nav>
+  );
+}
+export default HamburgerMenu;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,19 +1,33 @@
+import { useState } from "react";
+import HamburgerMenu from "./HamburgerMenu";
+
 function NavBar() {
+  const [isMenuShown, setIsMenuShown] = useState<boolean>(false);
+
   return (
-    // NOTE: Inversion is working but needs background elements
-    <header className="fixed w-full mix-blend-exclusion">
-      <nav className="flex justify-between px-4 py-6 items-center text-white *:uppercase">
-        <a href="#" className="w-min bold-text text-2xl leading-[0.75] ">
-          Ahmed Ibrahim
-        </a>
-        <a
-          href="#"
-          className="font-light relative after:content-['+'] after:absolute after:bottom-1/2 after:text-xs"
-        >
-          Menu
-        </a>
-      </nav>
-    </header>
+    <>
+      <header className="fixed w-full mix-blend-exclusion z-10">
+        <nav className="flex justify-between px-4 py-6 items-center text-white *:uppercase">
+          <a href="#" className="w-min bold-text text-2xl leading-[0.75] ">
+            Ahmed Ibrahim
+          </a>
+          <a
+            href="#"
+            className="font-light"
+            onClick={() => setIsMenuShown((prev) => !prev)}
+          >
+            {isMenuShown ? (
+              <span>[ close ]</span>
+            ) : (
+              <span className="relative after:content-['+'] after:absolute after:bottom-1/2 after:text-xs">
+                Menu
+              </span>
+            )}
+          </a>
+        </nav>
+      </header>
+      {isMenuShown && <HamburgerMenu />}
+    </>
   );
 }
 export default NavBar;
