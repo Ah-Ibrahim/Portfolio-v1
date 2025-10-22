7ec0e17c73aaafaa3b71cc2005c3a19a15a0daa9
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2025-10-22 22:48:53 +0300

    fix: make header inverted text over background elements

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,14 @@
 function NavBar() {
   return (
-    // TODO Remove white bg
-    <header className="bg-white fixed w-full">
-      <nav className="flex justify-between px-4 py-6 items-center text-white *:mix-blend-exclusion *:uppercase">
+    // NOTE: Inversion is working but needs background elements
+    <header className="fixed w-full mix-blend-exclusion">
+      <nav className="flex justify-between px-4 py-6 items-center text-white *:uppercase">
         <a href="#" className="w-min bold-text text-2xl leading-[0.75] ">
           Ahmed Ibrahim
         </a>
         <a
           href="#"
-          className="font-extralight relative after:content-['+'] after:absolute after:bottom-1/2 after:text-xs"
+          className="font-light relative after:content-['+'] after:absolute after:bottom-1/2 after:text-xs"
         >
           Menu
         </a>
