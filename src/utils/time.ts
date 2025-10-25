cf913ce3ad54572a469cbbad02208a3a19a0543e
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2025-10-25 23:57:00 +0300

    refactor: time util

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,20 +1,48 @@
-const region = "Africa/Cairo";
-const now = new Date();
+export interface TimeObject {
+  getTime: () => string;
+  getTimeZone: () => string;
+}
 
-const formatter = new Intl.DateTimeFormat("en-US", {
-  timeZone: region,
-  hour: "2-digit",
-  minute: "2-digit",
-  timeZoneName: "short",
-  hour12: false,
-});
+export function createTimeObject(): TimeObject {
+  const region = "Africa/Cairo";
 
-const parts = formatter.formatToParts(now);
-const time = parts
-  .filter((p) => p.type === "hour" || p.type === "minute" || p.value === ":")
-  .map((p) => p.value)
-  .join("");
+  const formatter = new Intl.DateTimeFormat("en-US", {
+    timeZone: region,
+    hour: "2-digit",
+    minute: "2-digit",
+    timeZoneName: "short",
+    hour12: false,
+  });
+  let parts = formatter.formatToParts(new Date());
+  let intervalId: number;
 
-const zone = parts.find((p) => p.type === "timeZoneName")?.value || "";
+  return {
+    // updateTime(callback: () => void) {
+    //   if (intervalId) throw Error("Already updating time");
 
-console.log(`${time} (${zone})`);
+    //   intervalId = setInterval(() => {
+    //     parts = formatter.formatToParts(new Date());
+    //     callback();
+    //   }, 1000);
+    // },
+    // stopUpdateTime() {
+    //   clearInterval(intervalId);
+    // },
+    getTime(): string {
+      parts = formatter.formatToParts(new Date());
+      const time = parts
+        .filter(
+          (p) => p.type === "hour" || p.type === "minute" || p.value === ":"
+        )
+        .map((p) => p.value)
+        .join("");
+
+      return time;
+    },
+    getTimeZone(): string {
+      parts = formatter.formatToParts(new Date());
+      const zone = parts.find((p) => p.type === "timeZoneName")?.value || "";
+      return zone;
+    },
+  };
+}
