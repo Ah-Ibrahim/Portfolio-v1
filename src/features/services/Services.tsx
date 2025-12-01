880ee4e01bb26eb1b9a99d1a85aa321345c9357a
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2025-12-01 02:41:39 +0200

    feat: add services section

---
+++ src/features/services/Services.tsx
@@ -0,0 +1,19 @@
+import Accordion from '@/components/common/Accordion';
+import servicesData from '@/data/services.json';
+import { ServiceSchema } from '@/lib/schemas/definitions';
+import z from 'zod';
+
+const services = z.array(ServiceSchema).parse(servicesData);
+
+function Services() {
+	return (
+		<section className="py-10">
+			<div className="flex justify-between px-4 mb-10 uppercase font-bold-condensed tracking-tighter">
+				<h2 className="text-5xl ">Services</h2>
+				<div className="text-sm">DSGN/4</div>
+			</div>
+			<Accordion panelsData={services} />
+		</section>
+	);
+}
+export default Services;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,16 +1,18 @@
-import NavBar from "@/components/layout/NavBar";
-import AboutMe from "@/features/about/AboutMe";
-import HeroSection from "@/features/hero/HeroSection";
-import Projects from "@/features/projects/Projects";
+import NavBar from '@/components/layout/NavBar';
+import AboutMe from '@/features/about/AboutMe';
+import HeroSection from '@/features/hero/HeroSection';
+import Projects from '@/features/projects/Projects';
+import Services from '@/features/services/Services';
 
 function HomePage() {
-  return (
-    <div className="page">
-      <NavBar />
-      <HeroSection />
-      <AboutMe />
-      <Projects />
-    </div>
-  );
+	return (
+		<div className="page">
+			<NavBar />
+			<HeroSection />
+			<AboutMe />
+			<Projects />
+			<Services />
+		</div>
+	);
 }
 export default HomePage;
