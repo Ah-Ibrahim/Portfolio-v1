632cc0853b48d4b12894c2d556791db3ca1401ae
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2025-12-01 02:42:21 +0200

    refactor: remove unused category property

--- src/lib/schemas/definitions.ts
+++ src/lib/schemas/definitions.ts
@@ -4,19 +4,18 @@ import z from "zod";
 const colorRegex = /^#([A-Fa-f0-9]{3}|[A-Fa-f0-9]{6})$/;
 
 const colorSchema = z.custom<`#${string}`>((val) =>
-  typeof val === "string" ? colorRegex.test(val) : false
+	typeof val === "string" ? colorRegex.test(val) : false
 );
 
 export const ProjectSchema = z
-  .object({
-    title: z.string(),
-    category: z.string(),
-    description: z.string(),
-    imageLink: z.url(),
-    previewLink: z.string(),
-    colorTheme: colorSchema,
-  })
-  .transform((obj) => ({ ...obj, id: nanoid() }));
+	.object({
+		title: z.string(),
+		description: z.string(),
+		imageLink: z.url(),
+		previewLink: z.string(),
+		colorTheme: colorSchema,
+	})
+	.transform((obj) => ({ ...obj, id: nanoid() }));
 
 export const ServiceSchema = z
 	.object({
@@ -31,11 +30,11 @@ export type ProjectType = z.infer<typeof ProjectSchema>;
 export type ServiceType = z.infer<typeof ServiceSchema>;
 
 export interface AccordionPanelData {
-  id: string;
-  title: string;
-  description: string;
-  imageLink: string;
-  previewLink?: string;
-  colorTheme?: string;
+	id: string;
+	title: string;
+	description: string;
+	imageLink: string;
+	previewLink?: string;
+	colorTheme?: string;
 	keywords?: string[];
 }
