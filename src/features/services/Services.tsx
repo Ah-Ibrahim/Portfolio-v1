8b00fe67255beaf17657af008f434bb92206ecfd
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2025-12-01 02:56:00 +0200

    fix: add services correct background color

--- src/features/services/Services.tsx
+++ src/features/services/Services.tsx
@@ -1,13 +1,13 @@
-import Accordion from '@/components/common/Accordion';
-import servicesData from '@/data/services.json';
-import { ServiceSchema } from '@/lib/schemas/definitions';
-import z from 'zod';
+import Accordion from "@/components/common/Accordion";
+import servicesData from "@/data/services.json";
+import { ServiceSchema } from "@/lib/schemas/definitions";
+import z from "zod";
 
 const services = z.array(ServiceSchema).parse(servicesData);
 
 function Services() {
 	return (
-		<section className="py-10">
+		<section className="py-10 bg-bg-primary">
 			<div className="flex justify-between px-4 mb-10 uppercase font-bold-condensed tracking-tighter">
 				<h2 className="text-5xl ">Services</h2>
 				<div className="text-sm">DSGN/4</div>
