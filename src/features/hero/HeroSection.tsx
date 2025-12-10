38442495bff1cf6a47fc651a32faaeecaa0e144f
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2025-12-11 00:34:54 +0200

    feat(responsiveness): add tablet and desktop support for hero section

--- src/features/hero/HeroSection.tsx
+++ src/features/hero/HeroSection.tsx
@@ -1,48 +1,62 @@
 import imageUrl from "@/assets/images/personal-image.jpeg";
 import ArrowIcon from "@/components/ui/ArrowIcon";
+import useMediaQuery from "@/hooks/useMediaQuery";
 
 function HeroSection() {
-	return (
-		<section className="bg-bg-primary pt-23 section-padding">
-			<div className="px-4">
-				<h1 className="text-[5.25rem] font-bold-condensed uppercase tracking-tighter leading-[0.75] text-right">
-					Creative Developer
-				</h1>
-				<div className="text-[0.70rem] uppercase flex justify-between *:tracking-[1em] translate-x-2">
-					<span>Based</span>
-					<span>in</span>
-					<span>Egypt</span>
-				</div>
-				<div className="flex mt-8 gap-x-1">
-					<div className="flex-5">
-						<img
-							src={imageUrl}
-							style={{ filter: "grayscale(80%)" }}></img>
-					</div>
-					<div className="uppercase font-bold-condensed mt-auto flex-4 *:w-max">
-						<div>/ Frontend developer</div>
-						<div>/ Computer Engineer</div>
-					</div>
-				</div>
-			</div>
-			<a
-				href="mailto:ahmed.ibrahim.elsayed01@gmail.com"
-				className="flex flex-col items-center mt-25">
-				<div className="w-max">
-					<span className=" uppercase font-light flex gap-x-1">
-						Available for collaboration <ArrowIcon />
-					</span>
-				</div>
-				{/* TODO: hover animation needs after and before underline instead of this   */}
-				<div className="font-bold-condensed underline self-end">
-					ahmed.ibrahim.elsayed01@gmail.com
-				</div>
-			</a>
-			{/* NOTE: Maybe About me should be added to About me section */}
-			<h2 className="uppercase font-bold-condensed text-[4.75rem] tracking-tighter text-center mt-10">
-				About me
-			</h2>
-		</section>
-	);
+  const isTabletOrLarger = useMediaQuery("(min-width:48rem)");
+
+  return (
+    <section className="bg-bg-primary pt-23 section-padding md:pb-4">
+      <div className="sm:max-md:px-4 flex flex-col isolate">
+        <h1 className="text-[5.25rem] font-bold-condensed uppercase tracking-tighter lg:-tracking-[0.0975em] leading-[0.75] sm:max-md:text-right md:text-[13vw] md:whitespace-nowrap md:text-center lg:text-[15.15vw] md:mt-6 text-right">
+          Creative Developer
+        </h1>
+        <div className="text-[0.70rem] uppercase flex justify-between *:tracking-[1em] translate-x-2 md:w-1/3 md:ms-auto font-bold-condensed">
+          <span>Based</span>
+          <span>in</span>
+          <span>Egypt</span>
+        </div>
+        <div className="flex mt-8 gap-x-1 md:gap-x-15 md:w-fit md:flex-row-reverse md:mx-auto md:bg-bg-accent md:pe-4 md:pb-4 -z-10">
+          <div className="flex-5 md:float-right">
+            <img
+              src={imageUrl}
+              style={{ filter: "grayscale(80%)" }}
+              className="md:scale-115 lg:scale-135 origin-bottom-right float-right"
+              alt="Ahmed Ibrahim"
+            ></img>
+          </div>
+          <div className="uppercase font-bold-condensed mt-auto flex-4 *:w-max">
+            <div>/ Frontend developer</div>
+            <div>/ Computer Engineer</div>
+          </div>
+        </div>
+      </div>
+      {isTabletOrLarger && (
+        <p className="text-center w-lg mx-auto mt-10 uppercase">
+          Turning ideas into clean, expressive interfaces — with a bit of
+          creative obsession. Every detail matters, and I aim to ship work
+          that’s intuitive, fast, and visually striking.
+        </p>
+      )}
+      <a
+        href="mailto:ahmed.ibrahim.elsayed01@gmail.com"
+        className="flex flex-col items-center mt-25x md:mt-20 md:items-start"
+      >
+        <div className="w-max">
+          <span className=" uppercase font-light flex gap-x-1">
+            Available for collaboration <ArrowIcon />
+          </span>
+        </div>
+        {/* TODO: hover animation needs after and before underline instead of this   */}
+        <div className="font-bold-condensed underline sm:max-md:self-end md:translate-x-1/2">
+          ahmed.ibrahim.elsayed01@gmail.com
+        </div>
+      </a>
+      {/* NOTE: Maybe About me should be added to About me section */}
+      <h2 className="uppercase font-bold-condensed text-[4.75rem] lg:text-9xl tracking-tighter text-center mt-10 md:mt-20">
+        About me
+      </h2>
+    </section>
+  );
 }
 export default HeroSection;
