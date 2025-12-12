f1a6e58b508b8a4e231d9bf705667e81d8409892
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2025-12-12 03:01:29 +0200

    fix (responsiveness): fix tablet and desktop support for hero section

--- src/features/hero/HeroSection.tsx
+++ src/features/hero/HeroSection.tsx
@@ -13,12 +13,12 @@ function HeroSection() {
         <h1 className="text-[5.25rem] font-bold-condensed uppercase tracking-tighter lg:-tracking-[0.0975em] leading-[0.75] sm:max-md:text-right md:text-[13vw] md:whitespace-nowrap md:text-center lg:text-[15.15vw] md:mt-6 text-right">
           Creative Developer
         </h1>
-        <div className="text-[0.70rem] uppercase flex justify-between *:tracking-[1em] translate-x-2 md:w-1/3 md:ms-auto font-bold-condensed">
+        <div className="text-[0.70rem] uppercase flex justify-between *:tracking-[1em] translate-x-2 md:w-1/3 md:ms-auto font-bold-condensed lg:w-1/4 lg:text-base">
           <span>Based</span>
           <span>in</span>
           <span>Egypt</span>
         </div>
-        <div className="flex mt-8 gap-x-1 md:gap-x-15 md:w-fit md:flex-row-reverse md:mx-auto md:bg-bg-accent md:pe-4 md:pb-4 -z-10">
+        <div className="flex mt-8 gap-x-1 md:gap-x-15 lg:gap-x-22 md:w-fit md:flex-row-reverse md:mx-auto md:bg-bg-accent md:pe-4 md:pb-4 -z-10">
           <div className="flex-5 md:float-right">
             <img
               src={imageUrl}
@@ -27,14 +27,14 @@ function HeroSection() {
               alt="Ahmed Ibrahim"
             ></img>
           </div>
-          <div className="uppercase font-bold-condensed mt-auto flex-4 *:w-max">
+          <div className="uppercase font-bold-condensed mt-auto flex-4 *:w-max md:text-lg lg:text-3xl">
             <div>/ Frontend developer</div>
             <div>/ Computer Engineer</div>
           </div>
         </div>
       </div>
       {isMd && (
-        <p className="text-center w-lg mx-auto mt-10 uppercase">
+        <p className="text-center w-lg mx-auto mt-10 uppercase font-light">
           Turning ideas into clean, expressive interfaces — with a bit of
           creative obsession. Every detail matters, and I aim to ship work
           that’s intuitive, fast, and visually striking.
@@ -42,7 +42,7 @@ function HeroSection() {
       )}
       <a
         href="mailto:ahmed.ibrahim.elsayed01@gmail.com"
-        className="flex flex-col items-center mt-25x md:mt-20 md:items-start"
+        className="flex flex-col items-center mt-25x md:mt-20 md:items-start md:text-lg lg:text-xl"
       >
         <div className="w-max">
           <span className=" uppercase font-light flex gap-x-1">
