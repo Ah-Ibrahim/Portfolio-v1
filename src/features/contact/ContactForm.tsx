90ea6689819244e2544caf25901f012682fae0f4
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2025-12-12 02:57:23 +0200

    feat(responsiveness): add tablet and desktop support for ContactForm

--- src/features/contact/ContactForm.tsx
+++ src/features/contact/ContactForm.tsx
@@ -3,82 +3,78 @@ import { useForm, ValidationError } from "@formspree/react";
 import { useRef } from "react";
 
 function ContactForm() {
-	const [state, handleSubmit] = useForm("mldqnezb");
-	const textAreaRef = useRef<HTMLTextAreaElement | null>(null);
+  const [state, handleSubmit] = useForm("mldqnezb");
+  const textAreaRef = useRef<HTMLTextAreaElement | null>(null);
 
-	if (state.succeeded) {
-		return (
-			<p className="uppercase font-bold-condensed my-6 text-center text-3xl">
-				Got it! 👍
-			</p>
-		);
-	}
+  if (state.succeeded) {
+    return (
+      <p className="uppercase font-bold-condensed my-6 text-center text-3xl">
+        Got it! 👍
+      </p>
+    );
+  }
 
-	const handleChange = () => {
-		const textAreaElement = textAreaRef.current;
+  const handleChange = () => {
+    const textAreaElement = textAreaRef.current;
 
-		if (!textAreaElement) return;
+    if (!textAreaElement) return;
 
-		textAreaElement.style.height = "auto";
-		textAreaElement.style.height = `${textAreaElement.scrollHeight}px`;
-	};
+    textAreaElement.style.height = "auto";
+    textAreaElement.style.height = `${textAreaElement.scrollHeight}px`;
+  };
 
-	return (
-		<form onSubmit={handleSubmit} className="flex flex-col py-6">
-			<input
-				id="name"
-				type="text"
-				name="name"
-				placeholder="Your name*"
-				className="border-b placeholder:uppercase py-2 "
-			/>
-			<div className="text-red-500 mt-1">
-				<ValidationError
-					prefix="Name"
-					field="name"
-					errors={state.errors}
-				/>
-			</div>
-			<input
-				id="email"
-				type="email"
-				name="email"
-				placeholder="Your email*"
-				className="border-b placeholder:uppercase py-2 mt-8"
-			/>
-			<div className="text-red-500 mt-1">
-				<ValidationError
-					prefix="Email"
-					field="email"
-					errors={state.errors}
-				/>
-			</div>
-			<textarea
-				id="message"
-				name="message"
-				placeholder="How can I help you?*"
-				className="border-b placeholder:uppercase py-2 resize-none mt-8"
-				ref={textAreaRef}
-				onChange={handleChange}
-				style={{
-					scrollbarWidth: "none",
-				}}
-			/>
-			<div className="text-red-500 mt-1">
-				<ValidationError
-					prefix="Message"
-					field="message"
-					errors={state.errors}
-				/>
-			</div>
-			<button
-				type="submit"
-				disabled={state.submitting}
-				className="uppercase cursor-pointer border-b flex self-center mt-10">
-				Discuss Project <ArrowIcon />
-			</button>
-		</form>
-	);
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col py-6 lg:w-xl lg:mx-auto"
+    >
+      <input
+        id="name"
+        type="text"
+        name="name"
+        placeholder="Your name*"
+        className="border-b placeholder:uppercase py-2 "
+      />
+      <div className="text-red-500 mt-1">
+        <ValidationError prefix="Name" field="name" errors={state.errors} />
+      </div>
+      <input
+        id="email"
+        type="email"
+        name="email"
+        placeholder="Your email*"
+        className="border-b placeholder:uppercase py-2 mt-8"
+      />
+      <div className="text-red-500 mt-1">
+        <ValidationError prefix="Email" field="email" errors={state.errors} />
+      </div>
+      <textarea
+        id="message"
+        name="message"
+        placeholder="How can I help you?*"
+        className="border-b placeholder:uppercase py-2 resize-none mt-8"
+        ref={textAreaRef}
+        onChange={handleChange}
+        style={{
+          scrollbarWidth: "none",
+        }}
+      />
+      <div className="text-red-500 mt-1">
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+        />
+      </div>
+      <button
+        type="submit"
+        disabled={state.submitting}
+        className="uppercase cursor-pointer border-b flex self-center mt-10 md:text-xl lg:mt-14 lg:text-2xl"
+      >
+        Discuss Project <ArrowIcon />
+      </button>
+    </form>
+  );
 }
 
 export default ContactForm;
