ccc9138d3d71a6bc65fe4138b1a2194cee178c47
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2025-12-12 02:51:33 +0200

    refactor: remove unused classname in item of LinkList

--- src/components/common/LinkList.tsx
+++ src/components/common/LinkList.tsx
@@ -1,21 +1,21 @@
 import type { link } from "@/lib/schemas/definitions";
 
 interface LinkListProps {
-	links: link[];
-	listStyle: string;
-	linkStyle: string;
+  links: link[];
+  listStyle: string;
+  linkStyle: string;
 }
 
 function LinkList({ links, linkStyle, listStyle }: LinkListProps) {
-	const items = links.map((link, index) => (
-		<li key={link.text + index}>
-			<a href={link.href} target="_blank" className={linkStyle}>
-				[<span className="text-xs">{link.text}</span>]
-			</a>
-		</li>
-	));
+  const items = links.map((link, index) => (
+    <li key={link.text + index}>
+      <a href={link.href} target="_blank" className={linkStyle}>
+        [<span className="">{link.text}</span>]
+      </a>
+    </li>
+  ));
 
-	return <ul className={listStyle}>{items}</ul>;
+  return <ul className={listStyle}>{items}</ul>;
 }
 
 export default LinkList;
