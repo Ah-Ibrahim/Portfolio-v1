c78773615b30689edf663dea14c0460a1076ca05
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2025-12-12 02:57:31 +0200

    feat(responsiveness): add tablet and desktop support for  Contact section

--- src/features/contact/Contact.tsx
+++ src/features/contact/Contact.tsx
@@ -1,22 +1,24 @@
 import ContactForm from "@/features/contact/ContactForm";
 
 function Contact() {
-	return (
-		<section className="section-padding bg-bg-primary space-y-4 py-10">
-			<div className="text-center tracking-[-0.2em] uppercase">
-				<div className="font-bold-condensed tracking-tighter">
-					Let's start the conversation
-				</div>
-				<div className="font-bold-condensed text-[2.75rem]">
-					Great Design
-				</div>
-				<div className="tracking-[0.75em] text-sm">Starts with</div>
-				<div className="font-bold-condensed text-[2.75rem]">
-					Great Collaboration
-				</div>
-			</div>
-			<ContactForm />
-		</section>
-	);
+  return (
+    <section className="section-padding bg-bg-primary space-y-4 lg:space-y-10 py-10 lg:py-16">
+      <div className="text-center tracking-[-0.2em] uppercase">
+        <div className="font-bold-condensed tracking-tighter lg:text-3xl">
+          Let's start the conversation
+        </div>
+        <div className="font-bold-condensed text-[2.75rem] lg:text-9xl">
+          Great Design
+        </div>
+        <div className="tracking-[0.75em] text-sm lg:text-base">
+          Starts with
+        </div>
+        <div className="font-bold-condensed text-[2.75rem] lg:text-9xl">
+          Great Collaboration
+        </div>
+      </div>
+      <ContactForm />
+    </section>
+  );
 }
 export default Contact;
