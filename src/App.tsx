614821a9f766a029bfe72f81ab86d79e5fe3924b
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2025-12-14 19:57:57 +0200

    feat: register GSAP plugins

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import HomePage from "@/pages/HomePage";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import CustomEase from "gsap/CustomEase";
+import { GSDevTools } from "gsap/GSDevTools";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/SplitText";
 import "./App.css";
+
+gsap.registerPlugin(useGSAP, GSDevTools, ScrollTrigger, SplitText, CustomEase);
 
 function App() {
   return (
