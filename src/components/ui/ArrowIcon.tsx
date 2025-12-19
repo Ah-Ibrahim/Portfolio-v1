78c480ed87f79796af24a23c33c82d47555c5d25
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2025-12-19 06:22:44 +0200

    fix: add missing duration for ArrowIcon

--- src/components/ui/ArrowIcon.tsx
+++ src/components/ui/ArrowIcon.tsx
@@ -4,7 +4,7 @@ function ArrowIcon() {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       fill="currentColor"
-      className="aspect-square w-5 group-hover:-rotate-45 transition-transform"
+      className="aspect-square w-5 group-hover:-rotate-45 transition-transform duration-400"
     >
       <path d="M4.2216 5.63589L5.63562 4.22168L17.0709 15.6569V10.0712H19.0709L19.0709 19.0712L10.0709 19.0712L10.0709 17.0712L15.6567 17.0712L4.2216 5.63589Z"></path>
     </svg>
