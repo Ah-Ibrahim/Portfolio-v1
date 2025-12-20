86248af9c573f8dca8665591222d95b351796cf9
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2025-12-21 01:16:02 +0200

    fix: add scroll trigger refresh on resizing or changing orientation

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,23 @@ import CustomEase from "gsap/CustomEase";
 import { GSDevTools } from "gsap/GSDevTools";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { SplitText } from "gsap/SplitText";
+import { useEffect } from "react";
 import "./App.css";
 
 gsap.registerPlugin(useGSAP, GSDevTools, ScrollTrigger, SplitText, CustomEase);
 
 function App() {
+  useEffect(() => {
+    const onResize = () => ScrollTrigger.refresh();
+    window.addEventListener("resize", onResize);
+    window.addEventListener("orientationchange", onResize);
+
+    return () => {
+      window.removeEventListener("resize", onResize);
+      window.removeEventListener("orientationchange", onResize);
+    };
+  }, []);
+
   return (
     <>
       <HomePage />
