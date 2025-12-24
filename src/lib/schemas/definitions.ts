3881dcaaf6841a6dea477818cce3e7ffe25f44d9
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2025-12-24 17:05:02 +0200

    chore(data): use static images for projects instead of github hosting

--- src/lib/schemas/definitions.ts
+++ src/lib/schemas/definitions.ts
@@ -4,42 +4,42 @@ import z from "zod";
 const colorRegex = /^#([A-Fa-f0-9]{3}|[A-Fa-f0-9]{6})$/;
 
 const colorSchema = z.custom<`#${string}`>((val) =>
-	typeof val === "string" ? colorRegex.test(val) : false
+  typeof val === "string" ? colorRegex.test(val) : false
 );
 
 export const ProjectSchema = z
-	.object({
-		title: z.string(),
-		description: z.string(),
-		imageLink: z.url(),
-		previewLink: z.string(),
-		colorTheme: colorSchema,
-	})
-	.transform((obj) => ({ ...obj, id: nanoid() }));
+  .object({
+    title: z.string(),
+    description: z.string(),
+    imageLink: z.string(),
+    previewLink: z.string(),
+    colorTheme: colorSchema,
+  })
+  .transform((obj) => ({ ...obj, id: nanoid() }));
 
 export const ServiceSchema = z
-	.object({
-		title: z.string(),
-		description: z.string(),
-		imageLink: z.url(),
-		keywords: z.array(z.string()),
-	})
-	.transform((obj) => ({ ...obj, id: nanoid() }));
+  .object({
+    title: z.string(),
+    description: z.string(),
+    imageLink: z.string(),
+    keywords: z.array(z.string()),
+  })
+  .transform((obj) => ({ ...obj, id: nanoid() }));
 
 export type ProjectType = z.infer<typeof ProjectSchema>;
 export type ServiceType = z.infer<typeof ServiceSchema>;
 
 export interface AccordionPanelData {
-	id: string;
-	title: string;
-	description: string;
-	imageLink: string;
-	previewLink?: string;
-	colorTheme?: string;
-	keywords?: string[];
+  id: string;
+  title: string;
+  description: string;
+  imageLink: string;
+  previewLink?: string;
+  colorTheme?: string;
+  keywords?: string[];
 }
 
 export interface link {
-	text: string;
-	href: string;
+  text: string;
+  href: string;
 }
