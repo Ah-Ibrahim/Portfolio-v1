32467f8db612f6042e689f1c8c0ce1140cf1f06e
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2026-01-11 00:42:22 +0200

    fix(responsiveness): make hero location text look consistent with hero main text Changed hero location text (Based in Egypt) to be relative to hero main text (Creative Developer).

--- src/features/hero/Hero.tsx
+++ src/features/hero/Hero.tsx
@@ -27,24 +27,25 @@ function Hero() {
       className="bg-bg-primary pt-23 section-padding pb-8 selection:text-white selection:bg-black"
     >
       <div className="sm:max-md:px-4 flex flex-col isolate">
-        <div className="overflow-hidden md:mt-6">
-          <h1
-            className="text-[4rem] xs:text-[5.25rem] font-bold-condensed uppercase tracking-tighter lg:-tracking-[0.0975em] leading-[0.75] md:text-[13vw] md:whitespace-nowrap md:text-center lg:text-[14vw] max-md:text-right"
-            id="hero-animation"
-          >
-            Creative Developer
+        <div className="md:mt-6">
+          <h1 className="text-[4rem] xs:text-[5.25rem] font-bold-condensed uppercase tracking-tighter lg:-tracking-[0.0975em] leading-[0.75] md:text-[13vw] md:whitespace-nowrap md:text-center lg:text-[14vw]">
+            <div className="w-min max-md:text-right max-md:ms-auto md:mx-auto relative">
+              <div id="hero-animation" className="overflow-y-clip">
+                Creative Developer
+              </div>
+              <div
+                className="text-[0.65rem] xs:text-[0.70rem] uppercase flex justify-between *:tracking-[1em] font-bold-condensed lg:w-1/3 lg:text-[0.8rem] xl:text-base absolute max-lg:left-0 right-0 top-full translate-x-3"
+                id="location-animation"
+              >
+                <span>Based</span>
+                <span>in</span>
+                <span>Egypt</span>
+              </div>
+            </div>
           </h1>
         </div>
         <div
-          className="text-[0.65rem] xs:text-[0.70rem] uppercase flex justify-between *:tracking-[1em] translate-x-2 md:w-1/3 md:ms-auto font-bold-condensed lg:w-1/4 xl:text-base invisible"
-          id="location-animation"
-        >
-          <span>Based</span>
-          <span>in</span>
-          <span>Egypt</span>
-        </div>
-        <div
-          className="flex mt-8 gap-x-1 md:gap-x-15 lg:gap-x-22 md:w-fit md:flex-row-reverse md:mx-auto md:bg-bg-accent md:pe-4 md:pb-4 -z-10 invisible"
+          className="flex mt-14 gap-x-1 md:gap-x-15 lg:gap-x-22 md:w-fit md:flex-row-reverse md:mx-auto md:bg-bg-accent md:pe-4 md:pb-4 -z-10 invisible"
           id="intro-animation"
         >
           <div className="flex-5 md:float-right">
