f4298988235e71a8092ddb848632d3b45fe7a65d
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2026-02-08 19:46:10 +0200

    refactor: use dynamic instead of React.lazy and Suspense

--- src/features/projects/ProjectsContent.tsx
+++ src/features/projects/ProjectsContent.tsx
@@ -3,27 +3,26 @@
 import AccordionSkeleton from "@/shared/components/common/Accordion/AccordionSkeleton";
 import { ProjectType } from "@/shared/lib/schemas/definitions";
 import dynamic from "next/dynamic";
-import { Suspense } from "react";
 import { useInView } from "react-intersection-observer";
-const Accordion = dynamic(
-  () => import("@/shared/components/common/Accordion/Accordion"),
-);
 
 function ProjectsContent({ projects }: { projects: ProjectType[] }) {
   const { ref, inView } = useInView({ triggerOnce: true, threshold: 0 });
+
+  const Accordion = dynamic(
+    () => import("@/shared/components/common/Accordion/Accordion"),
+    { loading: () => <AccordionSkeleton count={projects.length} /> },
+  );
 
   return (
     <section
       ref={ref}
       className="bg-bg-primary pt-4 pb-10 selection:text-white selection:bg-black"
     >
-      <Suspense fallback={<AccordionSkeleton count={projects.length} />}>
-        {inView ? (
-          <Accordion panelsData={projects} />
-        ) : (
-          <AccordionSkeleton count={projects.length} />
-        )}
-      </Suspense>
+      {inView ? (
+        <Accordion panelsData={projects} />
+      ) : (
+        <AccordionSkeleton count={projects.length} />
+      )}
     </section>
   );
 }
