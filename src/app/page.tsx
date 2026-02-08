f109e98be410386f65272c669ce501af6a58d91a
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2026-02-08 18:05:22 +0200

    feat: add Projects in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import AboutMe from "@/features/about/AboutMe";
 import Hero from "@/features/hero/Hero";
+import Projects from "@/features/projects/Projects";
 import NavBar from "@/shared/components/layout/NavBar";
 
 function page() {
@@ -9,6 +10,7 @@ function page() {
       <main>
         <Hero />
         <AboutMe />
+        <Projects />
       </main>
     </div>
   );
