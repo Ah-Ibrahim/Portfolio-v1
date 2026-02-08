7ce9ea5e2e1201111b060934888fe8b4062d196c
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2026-02-08 13:16:50 +0200

    feat: migrate LinkList

---
+++ src/shared/components/common/LinkList.tsx
@@ -0,0 +1,31 @@
+import type { link } from "@/shared/lib/schemas/definitions";
+
+interface LinkListProps {
+  links: link[];
+  listStyle: string;
+  linkStyle: string;
+  newTab?: boolean;
+}
+
+function LinkList({
+  links,
+  linkStyle,
+  listStyle,
+  newTab = false,
+}: LinkListProps) {
+  const items = links.map((link, index) => (
+    <li key={link.text + index}>
+      <a
+        href={link.href}
+        target={newTab ? "_blank" : "_self"}
+        className={linkStyle}
+      >
+        {link.text}
+      </a>
+    </li>
+  ));
+
+  return <ul className={listStyle}>{items}</ul>;
+}
+
+export default LinkList;
