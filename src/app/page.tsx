b295b8f76f5d4a9ae3f8d0305b7bbb2885803a53
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2026-02-08 17:44:03 +0200

    refactor: remove unused styles in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,8 @@ import Hero from "@/features/hero/Hero";
 import NavBar from "@/shared/components/layout/NavBar";
 
 function page() {
-  // NOTE h-screen & bg-white is for navbar preview, remove it after adding sections
   return (
-    <div className="page h-screen bg-white">
+    <div className="page">
       <NavBar />
       <main>
         <Hero />
