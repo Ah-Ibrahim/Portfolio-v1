ad6fd8df32566840027c0fc5ba7c3f959b288feb
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2026-02-08 21:17:08 +0200

    refactor: move scrubs of Hero into their components

--- src/features/hero/Hero.tsx
+++ src/features/hero/Hero.tsx
@@ -2,28 +2,14 @@
 
 import ArrowIcon from "@/shared/components/ui/ArrowIcon";
 import img from "@/../public/images/personal-image.webp";
-import useGSAPScrub from "@/shared/hooks/useGSAPScrub";
-import useMediaQuery from "@/shared/hooks/useMediaQuery";
-import screenBreakpoints from "@/shared/lib/breakpoints";
 import { useRef } from "react";
 import useIntroAnimation from "../intro-animation/useIntroAnimation";
+import Scrub from "./Scrub";
 
 function Hero() {
-  // Tablet or larger
-  const isMdOrLarger = useMediaQuery(`(min-width:${screenBreakpoints.md})`);
-
   const container = useRef<HTMLElement | null>(null);
 
-  // for useGSAPScrub
-  const scrubElement = useRef<HTMLHeadingElement>(null);
-  const scrubContainer = useRef<HTMLDivElement>(null);
-
-  // for large screen, be undefined to use useGSAPScrub default values
-  const startScrub = isMdOrLarger ? undefined : "start 65%";
-  const endScrub = isMdOrLarger ? undefined : "bottom 55%";
-
   useIntroAnimation(container);
-  useGSAPScrub(scrubElement, scrubContainer, startScrub, endScrub);
 
   return (
     <section
@@ -99,19 +85,7 @@ function Hero() {
           ahmed.ibrahim.elsayed01@gmail.com
         </div>
       </a>
-      <div
-        className="mt-15 md:mt-20 overflow-hidden"
-        id="about-container-animation"
-        ref={scrubContainer}
-      >
-        <h2
-          id="about-animation"
-          ref={scrubElement}
-          className="text-[3.5rem] uppercase font-bold-condensed xs:text-[4.75rem] tracking-tighter text-center  md:text-[11vw] lg:text-[13vw] leading-none invisible"
-        >
-          About me
-        </h2>
-      </div>
+      <Scrub />
     </section>
   );
 }
