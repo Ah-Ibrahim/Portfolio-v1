b0b681fbec1c79c437667b34313a3affac7738de
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2026-02-08 12:58:45 +0200

    fix: make useMediaQuery client's based

--- src/shared/hooks/useMediaQuery.ts
+++ src/shared/hooks/useMediaQuery.ts
@@ -1,20 +1,22 @@
+"use client";
+
 import { useEffect, useState } from "react";
 
 function useMediaQuery(query: string) {
-	const [matches, setMatches] = useState<boolean>(
-		() => window.matchMedia(query).matches
-	);
+  const [matches, setMatches] = useState<boolean>(false);
 
-	useEffect(() => {
-		const media = window.matchMedia(query);
+  useEffect(() => {
+    const media = window.matchMedia(query);
 
-		const updateHandler = () => setMatches(media.matches);
+    const updateHandler = () => setMatches(media.matches);
 
-		media.addEventListener("change", updateHandler);
+    updateHandler();
 
-		return () => media.removeEventListener("change", updateHandler);
-	}, [query]);
+    media.addEventListener("change", updateHandler);
 
-	return matches;
+    return () => media.removeEventListener("change", updateHandler);
+  }, [query]);
+
+  return matches;
 }
 export default useMediaQuery;
