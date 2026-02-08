8e1f045375985c5d675e2dae0b17679754a4884a
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2026-02-08 11:00:48 +0200

    feat: add fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,22 @@
 import type { Metadata } from "next";
+import { Sofia_Sans_Condensed } from "next/font/google";
+import { Spline_Sans_Mono } from "next/font/google";
 import "./global.css";
 
 export const metadata: Metadata = {
   title: "Ahmed Ibrahim",
   description: "Ahmed Ibrahim's Personal Portfolio",
 };
+
+const sofiaSansCondensed = Sofia_Sans_Condensed({
+  subsets: ["latin"],
+  variable: "--font-sofia-sans-condensed",
+});
+
+const splineSansMono = Spline_Sans_Mono({
+  subsets: ["latin"],
+  variable: "--font-spline-sans-mono",
+});
 
 export default function RootLayout({
   children,
@@ -16,7 +28,9 @@ export default function RootLayout({
       <head>
         <meta name="theme-color" content="#000" />
       </head>
-      <body>
+      <body
+        className={`${sofiaSansCondensed.variable} ${splineSansMono.variable} ${splineSansMono.className}`}
+      >
         <div id="root">{children}</div>
       </body>
     </html>
