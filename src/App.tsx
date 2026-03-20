1f18d36150185ad64e6b08888e3adb430ec74130
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2026-03-20 05:27:32 +0200

    refactor: move GSAP setup to a separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,13 @@
 import HomePage from "@/pages/HomePage";
-import { useGSAP } from "@gsap/react";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
-import gsap from "gsap";
-import CustomEase from "gsap/CustomEase";
-import { GSDevTools } from "gsap/GSDevTools";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { SplitText } from "gsap/SplitText";
-import { useEffect } from "react";
 import "./App.css";
-
-gsap.registerPlugin(useGSAP, GSDevTools, ScrollTrigger, SplitText, CustomEase);
+import GsapSetup from "./setup/gsap-setup";
 
 function App() {
-  useEffect(() => {
-    const onResize = () => ScrollTrigger.refresh();
-    window.addEventListener("resize", onResize);
-    window.addEventListener("orientationchange", onResize);
-
-    return () => {
-      window.removeEventListener("resize", onResize);
-      window.removeEventListener("orientationchange", onResize);
-    };
-  }, []);
-
   return (
     <>
+      <GsapSetup />
       <HomePage />
       <Analytics />
       <SpeedInsights />
